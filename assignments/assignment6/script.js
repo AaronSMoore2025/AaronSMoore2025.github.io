902d35abad0033151202faf3c39f84109ba48979
Aaron Moore <aaronmoore@Aarons-MacBook-Air-10.local>
2024-02-11 14:04:46 -0500

    pushing for check

--- assignments/assignment6/script.js
+++ assignments/assignment6/script.js
@@ -17,6 +17,29 @@ const pictureSwitch = () => {
 
 };
 
+const pictureSlide = () => {
+    const image = document.getElementById("exercise2Image");
+    const input = document.getElementById("exercise2Input").value;
+    if(input == 0){
+        image.src="images/yoga1.jpg";
+    } else if(input == 1){
+        image.src="images/yoga2.jpg";
+    } else if(input == 2) {
+        image.src="images/yoga3.jpg";
+    } else if(input == 3) {
+        image.src="images/yoga4.jpg";
+    } else if(input == 4) {
+        image.src="images/yoga5.jpg";
+    } else if(input == 5){
+        image.src="images/yoga6.jpg";
+    } else if(input == 6){
+        image.src="images/yoga7.jpg";
+    } else if(input == 7){
+        image.src="images/yoga8.jpg";
+    }
+};
+
+document.getElementById("exercise2Input").oninput = pictureSlide;
 
 
 const toggleHamburger = () => {
