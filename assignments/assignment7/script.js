70521a1de7c34ae08593c319cddd8fe00f7f39d0
Aaron Moore <aaronmoore@Aarons-MacBook-Air-10.local>
2024-02-15 16:12:56 -0500

    pushing problem

---
+++ assignments/assignment7/script.js
@@ -0,0 +1,23 @@
+const bounceBall = (event) => {
+    if(event.target.innerHTML.toLowerCase() == "start"){
+        event.target.innerHTML = "Stop";
+    } else {
+        event.target.innerHTML = "Start";
+        clearInterval(updateCount);
+    }
+
+    const ball = document.getElementById("soccerBall");
+    counter = 0;
+
+    const updateCount (()=>{
+        counter++;
+        ball.style.marginTop + "10px";
+
+        if(ball.marginTop == "300px"){
+            clearInterval(updateCount);
+        }
+    }, 100);
+
+};
+
+document.getElementyById("exercise1Button").onclick = bounceBall;
