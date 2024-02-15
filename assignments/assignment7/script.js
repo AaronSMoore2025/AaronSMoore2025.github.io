a26baaa0e529dae79dc006a4f64b4a4c5dd2f9e0
Aaron Moore <aaronmoore@Aarons-MacBook-Air-10.local>
2024-02-15 17:25:31 -0500

    hella confused

--- assignments/assignment7/script.js
+++ assignments/assignment7/script.js
@@ -1,3 +1,4 @@
+/*
 const bounceBall = (event) => {
     if(event.target.innerHTML.toLowerCase() == "start"){
         event.target.innerHTML = "Stop";
@@ -19,5 +20,25 @@ const bounceBall = (event) => {
     }, 500);
 
 };
+*/
 
+
+const showDetails = (event) => {
+    
+    /*
+    description.innerHTML = "hey how ya doin";
+    (event.target.getAttribute("rel"))";
+    */
+    
+   let description = event.target.getAttribute("rel)").value;
+   descriptionP.innerHTML = description;
+};
+
+
+/*
 document.getElementyById("exercise1Button").onclick = bounceBall;
+*/
+document.querySelectorAll("#poses li").forEach((li)=>{
+    li.onclick = showDetails
+    
+});
