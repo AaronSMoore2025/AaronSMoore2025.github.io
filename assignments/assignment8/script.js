6ecf6b7e9f46c0612668aa15846d056332e297bc
Aaron Moore <aaronmoore@Aarons-MacBook-Air-10.local>
2024-02-25 18:23:41 -0500

    final changes

--- assignments/assignment8/script.js
+++ assignments/assignment8/script.js
@@ -11,13 +11,30 @@ const switcher = () => {
     nextImage.classList.remove("hide");
 };
 
-/*
-$associatveArray = array(
-    "images/garden.jpg" => 
-    "images.golden.jpg" =>
-    "images/mountain-lake.jpg" =>
-    "small-house.jpg" =>
-    "snow.jpg" =>
-)
-*/
+const displayImages = () => {
+    let imageAttributes = [];
+    imageAttributes["images/garden.jpg"] = '<p><a href="https://www.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_11342065.htm#query=landscape&position=7&from_view=keyword&track=sph&uuid=16f8afcf-90c6-4cae-8249-a03fef90c6f4">Image by wirestock</a> on Freepik</p>';
+    imageAttributes["images/golden.jpg"] = '<p><a href="https://www.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_11342065.htm#query=landscape&position=7&from_view=keyword&track=sph&uuid=16f8afcf-90c6-4cae-8249-a03fef90c6f4"> Image by wirestock</a> on Freepik</p>';
+    imageAttributes["images/mountain-lake.jpg"] = '<p> <a href="https://www.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_40965130.htm#query=landscape&position=0&from_view=keyword&track=sph&uuid=8e520e53-3fb6-4e41-9da7-682c824a94f7"> Image by vecstock</a> on Freepik </p>';
+    imageAttributes["images/small-house.jpg"]  = '<p><a href="https://www.freepik.com/free-photo/small-houses-green-field-with-dark-sky_7553929.htm#query=landscape&position=39&from_view=keyword&track=sph&uuid=16f8afcf-90c6-4cae-8249-a03fef90c6f4">Image by wirestock</a> on Freepik</p>';
+    imageAttributes["images/snow.jpg"] = '<p><a href="https://www.freepik.com/free-photo/beautiful-scenery-lot-leafless-trees-snow-covered-land-during-sunset_10990489.htm#query=landscape&position=38&from_view=keyword&track=sph&uuid=16f8afcf-90c6-4cae-8249-a03fef90c6f4">Image by wirestock</a> on Freepik</p>'
+    
+
+
+    for(let imageAttribute in imageAttributes){
+        const holder = document.getElementById("image-holder");
+        const image = document.createElement("img");
+        const container = document.createElement("div");
+        image.src = imageAttribute;
+        container.append(image);
+        container.innerHTML += (imageAttributes[imageAttribute]);
+        container.classList.add("containerClass");
+        holder.appendChild(container);
+    };
+};
+
+
+window.onload = () => {
+    displayImages();
+};
 setInterval(switcher, 2000);
