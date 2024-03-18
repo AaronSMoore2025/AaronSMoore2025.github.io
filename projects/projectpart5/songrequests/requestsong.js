2a92c437f2304267bbdfabd80f9d7c4b91e0dacf
User <user@Glens-MacBook-Pro.local>
2024-03-18 17:44:54 -0400

    adding files for project part 5

---
+++ projects/projectpart5/songrequests/requestsong.js
@@ -0,0 +1,52 @@
+const getSongs = async() => {
+    const src = "requests.json";
+
+    try {
+        const response = await fetch(src);
+        return response.json();
+    } catch(error){
+        console.log(error);
+    }
+};
+
+const showSong = async() => {
+    const houses = await getSongs();
+    houses.forEach((house) => {
+        document.getElementById("songSection").append(geSongSection(house));
+    });
+};
+
+const getSongSection = (song) => {
+    const image = document.createElement("img");
+    image.id = "image";
+    const section = document.createElement("section");
+    section.id = "sectionOne";
+    const titleSection = document.createElement("section");
+    titleSection.id = "titleSection";
+    const informationSection = document.createElement("section");
+    informationSection.id = "informationSection";
+
+    const songImage = document.createElement("img");
+    songImage.innerHTML = song.image;
+    image.append(songImage);
+
+    const h1 = document.createElement("h1");
+    h1.innerHTML = song.name;
+    titleSection.append(h1);
+
+    const artist = document.createElement("p");
+    artist.innerHTML = song.artist;
+    informationSection.append(artist);
+
+    const genre = documnet.createElemement("p");
+    genre.innerHTML = song.genre;
+    informationSection.append(genre);
+
+    section.append(image);
+    section.append(titleSection);
+    section.append(informationSection);
+
+    return section;
+};
+
+showSong();
